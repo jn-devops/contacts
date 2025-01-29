aa4604d08d965cf0912d171c2cb016c1d64db29b
jn-devops <devops@joy-nostalg.com>
2025-01-29 21:39:51 +0800

    add code to employment type and employment status

--- src/Enums/EmploymentStatus.php
+++ src/Enums/EmploymentStatus.php
@@ -8,8 +8,8 @@ enum EmploymentStatus: string
 {
     use EnumUtils;
 
-    case REGULAR = 'Regular';
     case CONTRACTUAL = 'Contractual';
+    case REGULAR = 'Regular';
     case LOCALLY_EMPLOYED = 'Locally Employed';
     case OFW = 'Overseas Filipino Worker (OFW)';
     case SELF_EMPLOYED = 'Self Employed with Business';
@@ -17,5 +17,16 @@ enum EmploymentStatus: string
 
     static function default(): self {
         return self::REGULAR;
+    }
+
+    public function code(): string
+    {
+        return match($this) {
+            self::CONTRACTUAL => '001',
+            self::REGULAR => '002',
+            self::LOCALLY_EMPLOYED => '003',
+            self::OFW => '004',
+            self::SELF_EMPLOYED => '005',
+        };
     }
 }
--- src/Enums/EmploymentType.php
+++ src/Enums/EmploymentType.php
@@ -15,4 +15,22 @@ enum EmploymentType: string
     static function default(): self {
         return self::LOCALLY_EMPLOYED;
     }
+
+    public function sync_id(): string
+    {
+        return match($this) {
+            self::LOCALLY_EMPLOYED => '001',
+            self::SELF_EMPLOYED => '002',
+            self::OFW => '003'
+        };
+    }
+
+    public function code(): string
+    {
+        return match($this) {
+            self::LOCALLY_EMPLOYED => '001',
+            self::SELF_EMPLOYED => '002',
+            self::OFW => '003'
+        };
+    }
 }
