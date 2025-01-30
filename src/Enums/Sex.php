e609cac8e164404b6bb49be2fd8e8a79bc7255da
jn-devops <devops@joy-nostalg.com>
2025-01-30 21:46:59 +0800

    add other function to sex enum

--- src/Enums/Sex.php
+++ src/Enums/Sex.php
@@ -10,4 +10,9 @@ enum Sex: string
 
     case MALE = 'Male';
     case FEMALE = 'Female';
+
+    public function other(): self
+    {
+        return $this === self::MALE ? self::FEMALE : self::MALE;
+    }
 }
