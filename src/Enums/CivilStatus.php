4af22dc09bf4aa66823b6be0ff6ebde702b02d33
jn-devops <devops@joy-nostalg.com>
2025-01-30 16:52:30 +0800

    add has code to civil status

--- src/Enums/CivilStatus.php
+++ src/Enums/CivilStatus.php
@@ -3,10 +3,12 @@
 namespace Homeful\Contacts\Enums;
 
 use Homeful\Common\Traits\EnumUtils;
+use Homeful\Contacts\Traits\HasCode;
 
 enum CivilStatus: string
 {
     use EnumUtils;
+    use HasCode;
 
     case SINGLE = 'Single';
     case MARRIED = 'Married';
@@ -15,8 +17,19 @@ enum CivilStatus: string
     case SEPARATED = 'Separated';
     case WIDOW_ER = 'Widow/er';
 
-
     static function default(): self {
         return self::MARRIED;
+    }
+
+    public function code(): string
+    {
+        return match($this) {
+            self::SINGLE => '001',
+            self::MARRIED => '002',
+            self::WIDOWED => '003',
+            self::DIVORCED => '004',
+            self::SEPARATED => '005',
+            self::WIDOW_ER => '006',
+        };
     }
 }
