eb8b40bbb72620b0d1cd2321452a3e1dc8a66eda
jn-devops <devops@joy-nostalg.com>
2025-02-21 16:16:52 +0800

    fix $this->civil_status

--- src/Classes/ContactMetaData.php
+++ src/Classes/ContactMetaData.php
@@ -58,11 +58,18 @@ class ContactMetaData extends Data
         $this->name = implode(' ', array_filter([$first_name, $middle_name, $last_name, $name_suffix?->value]));
         $this->monthly_gross_income = $this->getMonthlyGrossIncome();
 
-        if ($this->civil_status instanceof CivilStatus)
-            $this->civil_connection = $this->civil_status == CivilStatus::MARRIED
-                ? $this->civil_status->value . ' to '
-                : $this->civil_status->value
-            ;
+    // Extract the actual CivilStatus value from $this->civil_status
+        $civil = $this->civil_status instanceof CivilStatus
+            ? $this->civil_status
+            : ($this->civil_status instanceof Optional
+                ? $this->civil_status->value
+                : null);
+
+        if ($civil instanceof CivilStatus) {
+            $this->civil_connection = $civil === CivilStatus::MARRIED
+                ? $civil->value . ' to '
+                : $civil->value;
+        }
     }
 
     public static function prepareForPipeline($properties): array
