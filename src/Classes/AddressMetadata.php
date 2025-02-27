70ed218e4b842ea2c06b99ef84636dd04ff121db
Christian Quelonio <cequelonio@joy-nostalg.com>
2025-02-27 15:49:52 +0800

    added sublocality in addresses

--- src/Classes/AddressMetadata.php
+++ src/Classes/AddressMetadata.php
@@ -21,13 +21,14 @@ class AddressMetadata extends Data
         #[WithCast(EnumCast::class)]
         public Ownership $ownership,
         public ?string $address1,
+        public ?string $sublocality,
         public ?string $locality,
         public ?string $administrative_area,
         public ?string $postal_code,
         public ?string $region,
         public ?string $country
     ) {
-        $this->address = implode(', ', array_filter([$this->address1, $this->locality, $this->administrative_area, $this->postal_code]));
+        $this->address = implode(', ', array_filter([$this->address1, $this->sublocality, $this->locality, $this->administrative_area, $this->postal_code]));
         $this->short_address = '156 Soliven St., Pasig Green Park Village, Barangay Manggahan, Pasig City 1605';
     }
 }
